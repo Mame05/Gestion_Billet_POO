2b3ad02532f6b3253234a088465bdd8c7e672e27
mareme <mariamathiaw92@gmail.com>
2024-04-08 00:08:38 +0000

    Implémentation de l'interface du crud du billet

---
+++ crudBillet.php
@@ -0,0 +1,9 @@
+<?php
+interface CRUD
+{
+    public function createBillet($depart,$id_destination,$date_depart,$date_arrivee,$prix,$statut,$date_reservation,$id_client);
+    public function readBillet();
+    public function updateBillet($id,$depart,$id_destination,$date_depart,$date_arrivee,$prix,$statut,$date_reservation);
+    public function deleteBillet($id);
+}
+?>
